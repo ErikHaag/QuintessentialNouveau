eb15729f996ed91f0c08d96cfe0f1758c9edf130
Erik <EH2004625@gmail.com>
2026-03-22 22:17:19 -0500

    The game no longer immediately crashes

---
+++ Patches/patch_WorkshopManager.cs
@@ -0,0 +1,13 @@
+﻿class patch_WorkshopManager
+{
+    public extern void orig_method_670();
+
+    // the valve's been stuck, corroded by the money.
+    public void method_670()
+    {
+        ((WorkshopManager)(object)this).method_2303();
+        ((WorkshopManager)(object)this).method_2304();
+    }
+
+
+}
